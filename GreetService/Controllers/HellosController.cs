c6a3aafc5e8b3b357f41d8760a8cd0c3abc90e48
solesen1992 <saramundal@live.dk>
2024-10-07 11:07:46 +0200

    Add project files.

---
+++ GreetService/Controllers/HellosController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GreetService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HellosController : ControllerBase
+    {
+        [HttpGet]
+        [Produces("application/json")]
+        [ProducesResponseType(StatusCodes.Status200OK)] // Can also add that the type is string.
+        public ActionResult<string> Get() {
+            //return "hello";
+            //return Ok("hi");
+            return BadRequest();
+        }
+    }
+}
